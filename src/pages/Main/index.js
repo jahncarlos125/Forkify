7720e2afc5db2c781b0a367693a3c2b0e6880927
Jahn Carlos <jahn.santana@gmail.com>
2020-02-17 20:04:20 -0300

    stylize inputText

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Form, Input, SubmitButton} from './styles';
 
-const Main = () => <View />;
+export default function Main() {
+  return (
+    <Container>
+      <Form>
+        <Input placeholder="Buscar receitas.." />
+        <SubmitButton>
+          <Icon name="search" size={20} color="#eee" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
 
-export default Main;
+Main.navigationOptions = {
+  title: 'Forkify - Receitas',
+  headerStyle: {
+    backgroundColor: '#FFF',
+  },
+  headerTintColor: '#7159c1',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
